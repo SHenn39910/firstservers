b51ad9191a9fa01c9698722f00f8304f8affeff0
Sonja Hennessy <sonjahennessy15@gmail.com>
2021-06-04 12:23:30 -0400

    created catch err

--- first-servers/htmlFile.js
+++ first-servers/htmlFile.js
@@ -26,9 +26,16 @@ const requestListener= function(req, res){
 //actual server with port
 
 const server = http.createServer(requestListener);
-server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
+fs.readFile(__dirname + "/index.html")
+    .then(contents => {
+        indexFile = contents;
+        server.listen(port, host, () => {
+            console.log(`Server is running on http://${host}:${port}`);
+    });
+})
+.catch(err => {
+    console.error(`Could not read index.html file: ${err}`);
+    process.exit(1);
 });
-
 
 
